89da79c429f39f80b68c024a02d1e88bc8bc473a
rafaelgit85 <you@example.com>
2020-07-21 22:01:14 -0300

    first age calculator

--- prjCppOperadoresBasicos/prjCppOperadoresBasicos/main.cpp
+++ prjCppOperadoresBasicos/prjCppOperadoresBasicos/main.cpp
@@ -4,8 +4,20 @@ using namespace std;
 
 void main() {
 
+	string name;
+	short bYear, currentYear = 2020, age;
 
 
+	cout << "  AGE CALCULATOR"<<endl;
+	cout << "  --------------" << endl;
+	cout << "Please, enter your Name: ";
+	cin >> name;
+	cout << "Enter your year of birth: ";
+	cin >> bYear;
+
+	age = currentYear - bYear;
+
+	cout << "\nSir or Miss " << name << ", you are " << age << " years old.";
 
 
 
